6a9961abf9f19e88663c051ed58029b028c291c1
Ashraf <ashrafali.11810784@gmail.com>
2021-02-21 20:27:35 +0530

    First Program

---
+++ p1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void inputData()
+{
+	int a;
+	string b;
+	cin>>a>>b;
+	cout<<a<<" "<<b<<endl;
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		inputData();
+	}
+}
